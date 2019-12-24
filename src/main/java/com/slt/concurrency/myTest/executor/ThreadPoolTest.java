aacb32760b51023874bbf656a96b9f2828887dd0
yuyanan <yuyanan@handongkeji.com>
2019-12-24 14:49:40 +0800

    doc

---
+++ src/main/java/com/slt/concurrency/myTest/executor/ThreadPoolTest.java
@@ -0,0 +1,20 @@
+package com.slt.concurrency.myTest.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolTest {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            executor.submit(() -> {
+                System.out.println("thread id is: " + Thread.currentThread().getId());
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+}
