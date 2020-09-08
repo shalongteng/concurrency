79db054ca75a02fac1a79c59011d549c054026c5
shalongteng <1131790681@qq.com>
2020-09-08 22:28:50 +0800

    0

--- src/main/java/com/slt/concurrency/book1/ch3/jmh/JMHSample_02_BenchmarkModes.java
+++ src/main/java/com/slt/concurrency/book1/ch3/jmh/JMHSample_02_BenchmarkModes.java
@@ -1,84 +1,84 @@
-package com.slt.concurrency.book1.ch3.jmh;
-
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
-import java.util.concurrent.TimeUnit;
-
-public class JMHSample_02_BenchmarkModes {
-
-    /**
-     * 测试吞吐量
-     */
-    @Benchmark
-    @BenchmarkMode(Mode.Throughput)
-    @OutputTimeUnit(TimeUnit.SECONDS)
-    public void measureThroughput() throws InterruptedException {
-        TimeUnit.MILLISECONDS.sleep(100);
-    }
-
-    /**
-     * 统计方法的平均执行时间的设置如下 ：
-     */
-    @Benchmark
-    @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    public void measureAvgTime() throws InterruptedException {
-        TimeUnit.MILLISECONDS.sleep(100);
-    }
-
-    /**
-     * 通过采样得到部分方法的执行时间，：
-     */
-    @Benchmark
-    @BenchmarkMode(Mode.SampleTime)
-    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void measureSamples() throws InterruptedException {
-        TimeUnit.MILLISECONDS.sleep(100);
-    }
-
-    /*
-     */
-    @Benchmark
-    @BenchmarkMode(Mode.SingleShotTime)
-    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void measureSingleShot() throws InterruptedException {
-        TimeUnit.MILLISECONDS.sleep(100);
-    }
-
-    /*
-     * We can also ask for multiple benchmark modes at once. All the tests above can
-     * be replaced with just a single test like this:
-     */
-    @Benchmark
-    @BenchmarkMode({ Mode.Throughput, Mode.AverageTime, Mode.SampleTime, Mode.SingleShotTime })
-    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void measureMultiple() throws InterruptedException {
-        TimeUnit.MILLISECONDS.sleep(100);
-    }
-
-    /*
-     * Or even...
-     */
-
-    @Benchmark
-    @BenchmarkMode(Mode.All)
-    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void measureAll() throws InterruptedException {
-        TimeUnit.MILLISECONDS.sleep(100);
-    }
-
-    /*
-     */
-    public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder().include(JMHSample_02_BenchmarkModes.class.getSimpleName()).forks(1).build();
-
-        new Runner(opt).run();
-    }
-
-}
+//package com.slt.concurrency.book1.ch3.jmh;
+//
+//import org.openjdk.jmh.annotations.Benchmark;
+//import org.openjdk.jmh.annotations.BenchmarkMode;
+//import org.openjdk.jmh.annotations.Mode;
+//import org.openjdk.jmh.annotations.OutputTimeUnit;
+//import org.openjdk.jmh.runner.Runner;
+//import org.openjdk.jmh.runner.RunnerException;
+//import org.openjdk.jmh.runner.options.Options;
+//import org.openjdk.jmh.runner.options.OptionsBuilder;
+//import java.util.concurrent.TimeUnit;
+//
+//public class JMHSample_02_BenchmarkModes {
+//
+//    /**
+//     * 测试吞吐量
+//     */
+//    @Benchmark
+//    @BenchmarkMode(Mode.Throughput)
+//    @OutputTimeUnit(TimeUnit.SECONDS)
+//    public void measureThroughput() throws InterruptedException {
+//        TimeUnit.MILLISECONDS.sleep(100);
+//    }
+//
+//    /**
+//     * 统计方法的平均执行时间的设置如下 ：
+//     */
+//    @Benchmark
+//    @BenchmarkMode(Mode.AverageTime)
+//    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+//    public void measureAvgTime() throws InterruptedException {
+//        TimeUnit.MILLISECONDS.sleep(100);
+//    }
+//
+//    /**
+//     * 通过采样得到部分方法的执行时间，：
+//     */
+//    @Benchmark
+//    @BenchmarkMode(Mode.SampleTime)
+//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+//    public void measureSamples() throws InterruptedException {
+//        TimeUnit.MILLISECONDS.sleep(100);
+//    }
+//
+//    /*
+//     */
+//    @Benchmark
+//    @BenchmarkMode(Mode.SingleShotTime)
+//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+//    public void measureSingleShot() throws InterruptedException {
+//        TimeUnit.MILLISECONDS.sleep(100);
+//    }
+//
+//    /*
+//     * We can also ask for multiple benchmark modes at once. All the tests above can
+//     * be replaced with just a single test like this:
+//     */
+//    @Benchmark
+//    @BenchmarkMode({ Mode.Throughput, Mode.AverageTime, Mode.SampleTime, Mode.SingleShotTime })
+//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+//    public void measureMultiple() throws InterruptedException {
+//        TimeUnit.MILLISECONDS.sleep(100);
+//    }
+//
+//    /*
+//     * Or even...
+//     */
+//
+//    @Benchmark
+//    @BenchmarkMode(Mode.All)
+//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+//    public void measureAll() throws InterruptedException {
+//        TimeUnit.MILLISECONDS.sleep(100);
+//    }
+//
+//    /*
+//     */
+//    public static void main(String[] args) throws RunnerException {
+//        Options opt = new OptionsBuilder().include(JMHSample_02_BenchmarkModes.class.getSimpleName()).forks(1).build();
+//
+//        new Runner(opt).run();
+//    }
+//
+//}
--- src/main/java/com/slt/concurrency/mashibing2019/juc/c_020_LockAndTools/T11_TestSemaphore.java
+++ src/main/java/com/slt/concurrency/mashibing2019/juc/c_020_LockAndTools/T11_TestSemaphore.java
@@ -5,16 +5,17 @@ import java.util.concurrent.Semaphore;
 public class T11_TestSemaphore {
     public static void main(String[] args) {
         //Semaphore s = new Semaphore(2);
-        Semaphore s = new Semaphore(2, true);
+        Semaphore s = new Semaphore(1, true);
         //允许一个线程同时执行
         //Semaphore s = new Semaphore(1);
 
         new Thread(()->{
             try {
+                //获取许可
                 s.acquire();
 
                 System.out.println("T1 running...");
-                Thread.sleep(200);
+                Thread.sleep(2000);
                 System.out.println("T1 running...");
 
             } catch (InterruptedException e) {
@@ -29,7 +30,7 @@ public class T11_TestSemaphore {
                 s.acquire();
 
                 System.out.println("T2 running...");
-                Thread.sleep(200);
+                Thread.sleep(2000);
                 System.out.println("T2 running...");
 
                 s.release();
